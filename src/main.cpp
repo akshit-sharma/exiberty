663597462ffb2d4fc423eb4c0c63ba255aa18096
akshit-sharma <akshit9sharma@gmail.com>
2019-07-22 22:35:59 -0600

    still working on a way to properly import

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 
+#include "libiberty.h"
+
 int main(int argc, char *argv[]) {
 
   std::cout << "Hello World\n";
+
+  std::cout << "Current pwd is " << getpwd << std::endl;
 
   return 0;
 }
