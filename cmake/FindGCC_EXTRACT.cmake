96102e895abf3a81b1228f040b9a7ea1f9c89c49
akshit-sharma <akshit9sharma@gmail.com>
2019-08-24 13:17:29 -0600

    fixed code to compile maybe

--- cmake/FindGCC_EXTRACT.cmake
+++ cmake/FindGCC_EXTRACT.cmake
@@ -8,11 +8,19 @@ include(FindPackageHandleStandardArgs)
 find_path(GCC_IBERTY_INCLUDE_DIRS libiberty.h
   HINTS ${CMAKE_CURRENT_SOURCE_DIR}/../include 
   )
-find_library(GCC_IBERTY_LIBRARY iberty
+find_library(GCC_IBERTY_LIBRARY 
+  NAMES iberty
+  libiberty
+  libiberty.a
   HINTS ${CMAKE_CURRENT_SOURCE_DIR}/../libiberty
   ) # ${CMAKE_CURRENT_BINARY_DIR}/.../src/.../libiberty.a)
 find_package_handle_standard_args(GCC_EXTRACT::iberty
   REQUIRED_VARS GCC_IBERTY_DIRS GCC_IBERTY_LIBRARY)
+
+message(STATUS "value are ${CMAKE_CURRENT_SOURCE_DIR} and ${CMAKE_CURRENT_BINARY_DIR}")
+
+message(STATUS "Value is ${GCC_IBERTY_INCLUDE_DIRS} and ${GCC_IBERTY_LIBRARY}")
+
 add_library(GCC_EXTRACT::iberty UNKOWN IMPORTED)
 set_target_properties(GCC_EXTRACT::iberty PROPERTIES
   IMPORTED_LOCATION             ${GCC_IBERTY_LIBRARY}
